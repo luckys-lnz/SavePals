c6759f84e59ace7d1bc021d5d9f60015883d5ad0
luckys-lnz <lalucky22@gmail.com>
2024-08-23 09:46:39 +0000

    this is a setup file for the mysql-database.

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- SQL DATABASE SETUP FOR SavePals;
+
+CREATE DATABASE IF NOT EXISTS SavePals;
+CREATE USER IF NOT EXISTS 'savepals_dev'@'localhost' IDENTIFIED BY 'savepals_dev_pwd';
+GRANT ALL PRIVILEGES ON 'SavePals'.* TO 'savepals_dev'@'localhost';
+GRANT ALL SELECT ON 'performance_schema'.* To 'savepals_dev'@'localhost';
+FLUSH PRIVILEGES;
