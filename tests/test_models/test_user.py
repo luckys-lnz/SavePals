7ab2b86f882c7439b16a0bac43291f6764b311aa
deantosh <deantoshdaiddoh@gmail.com>
2024-08-24 03:16:44 -0700

    test: Add tests for User class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import time
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+"""
+Module defines a test for the `User` class
+"""
+
+
+class TestUser(unittest.TestCase):
+    """
+    Test User class
+    """
+
+    def setUp(self):
+        """ Setup test environment """
+        self.user = User()
+
+    def test_class_attributes(self):
+        """ Test class attributes """
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+        self.assertEqual(self.user.user_name, "")
+
+    def test_class_attributes_type(self):
+        """ Test class attributes type """
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+
+    def test_inherits_from_base_model(self):
+        """ Test class inherits from `BaseModel` class """
+        self.assertIsInstance(self.user, BaseModel)
