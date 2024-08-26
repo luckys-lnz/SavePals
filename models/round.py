1fcc28173df3204156e05e5365d7ec749cca6094
deantosh <deantoshdaiddoh@gmail.com>
2024-08-26 12:55:38 -0700

    class: Round -- round model

---
+++ models/round.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a Round class
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Round(BaseModel, Base):
+    """This class defines a round by various attributes"""
+
+    __tablename__ = 'rounds'
+    round_number = Column(String(128), nullable=False)
+    amount = Column(String(128), nullable=False)
+    group_id = Column(String(60), ForeignKey('groups.id'),
+                      nullable=False)
