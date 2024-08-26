44e977a711f5bbcfe7459c82a113f441c34304a9
deantosh <deantoshdaiddoh@gmail.com>
2024-08-26 12:54:43 -0700

    user_grop assciation table

---
+++ models/user_group.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a user_group table
+"""
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from models.base_model import Base
+
+
+# Association table for many-to-many relationship between User and Group
+user_group_association = Table(
+    'user_group',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'),
+           primary_key=True),
+    Column('group_id', Integer, ForeignKey('groups.id'),
+           primary_key=True),
+    Column('is_admin', Boolean, nullable=False, default=False),
+    Column('joined_at', DateTime, nullable=False,
+           default=datetime.utcnow)
+)
