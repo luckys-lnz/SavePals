42f0a71c7c9ba5f92f134b7481550f0ef6c98520
deantosh <deantoshdaiddoh@gmail.com>
2024-08-29 10:19:35 -0700

    initializes package

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ This module instantiates an object of class DBStorage
 from models.base_model import Base
 from models.user import User
 from models.group import Group
-from models.round import Round
+from models.round_ import Round
 from models.payout import Payout
 from models.contribution import Contribution
 from models.engine.db_storage import DBStorage
