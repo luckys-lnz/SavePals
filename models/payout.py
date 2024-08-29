932fab2ecde9872bc3adb467e4bd3d64345c945d
deantosh <deantoshdaiddoh@gmail.com>
2024-08-29 10:25:55 -0700

    Model: Payout class

--- models/payout.py
+++ models/payout.py
@@ -5,9 +5,7 @@ Round table holds data for next collector in a group.
 
 Base on the plan agreement.
 """
-
-import models
-from models.engine import DBstorage
+from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
@@ -15,24 +13,10 @@ from sqlalchemy.orm import relationship
 
 class Payout(BaseModel, Base):
     """Representation of the user turns table."""
+    __tablename__ = 'payout'
 
-    if models.storage_t == 'db':
-        __tablename__ = 'payout'
+    #table colums
+    round_id = Column(String, ForeignKey('rounds.id'), nullable=False)
+    user_id = Column(String, ForeignKey('users.id'), nullable=False)
+    amount = Column(Float, nullable=False)
 
-        #table colums
-        id = Column(Integer, primary_key=True)
-        round_id = Column(Integer, ForeignKey=('round.id'), nullable=False)
-        user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-        amount = Column(Float, nullable=False)
-        payment_date = Column(sqlalchemy.DateTime, nullable=False,
-                              default=sqlalchemy.funct.now())
-
-        # Table Relationships
-        round = relationship('Round', back_populates='payouts')
-        user = relationship('User', back_populates='payouts')
-
-    else:
-        round_id = 0
-        user_id = 0
-        amount = 0.00
-        payment_date = None
