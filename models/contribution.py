60853201c6feafba2c1c46455f67fc8f747f1d02
luckys-lnz <lalucky22@gmail.com>
2024-09-02 19:31:55 +0000

    contributions resolved conflict

---
+++ models/contribution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module defines a `Contribution` class.
+"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+
+class Contribution(BaseModel, Base):
+    """Representation of the user Contribution table."""
+    __tablename__ = 'contributions'
+
+    # table column
+    group_id = Column(String, ForeignKey('groups.id'), nullable=False)
+    round_id = Column(String, ForeignKey('rounds.id'), nullable=False)
+    user_id = Column(String, ForeignKey('users.id'), nullable=False)
+    amount = Column(Float, nullable=False)
