838a615670ffceb08ea3dab5218ebd5b02eecc36
lucky Archibong <Lalucky22@gmail.com>
2024-09-02 19:36:40 +0000

    Update Contribution.py

--- models/Contribution.py
+++ models/Contribution.py
@@ -1,43 +1,20 @@
 #!/usr/bin/python3
 
 """
-Round table holds data for next collector in a group.
-
-Base on the plan agreement.
+Module defines a `Contribution` class.
 """
 
-import models
-from models.engine import DBstorage
-import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
 class Contribution(BaseModel, Base):
     """Representation of the user Contribution table."""
+    __tablename__ = 'contributions'
 
-    if models.storage_t == 'db':
-        __tablename__ = 'contribution'
-
-        # table colums
-        id = Column(Integer, primary_key=True)
-        round_id = Column(Integer, ForeignKey=('round.id'), nullable=False)
-        user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-        amount = Column(Float, nullable=False)
-        payment_date = Column(sqlalchemy.DateTime, nullable=False,
-                              default=sqlalchemy.funct.now())
-
-        # Table Relationships
-        round = relationship('Round', back_populates='contributions')
-        user = relationship('User', back_populates='contributions')
-
-    else:
-        round_id = 0
-        user_id = 0
-        amount = 0.00
-        payment_date = None
-
-
-def __repr__(self):
-    return "<Contribution(id='%i', round_id='%i', amount='%i')>" % (
-                              self.id, self.round_id, self.amount)
+    # table column
+    group_id = Column(String, ForeignKey('groups.id'), nullable=False)
+    round_id = Column(String, ForeignKey('rounds.id'), nullable=False)
+    user_id = Column(String, ForeignKey('users.id'), nullable=False)
+    amount = Column(Float, nullable=False)
