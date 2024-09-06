759fae40d501b10fc291d63b3e21bd9d4a0faed2
bobuzy <bobuzytech@gmail.com>
2024-09-06 11:19:09 +0000

    Modified dashboard stylesheet link

--- web_dynamic/static/scripts/navbar.js
+++ web_dynamic/static/scripts/navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', '../../components/navbar.html', true);
+    xhr.open('GET', '../../templates/navbar.html', true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             document.getElementById('navbar-placeholder').innerHTML = xhr.responseText;
