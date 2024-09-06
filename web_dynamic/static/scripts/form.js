62b05c71f12f9479a3b146e3351ef345b01ca53c
bobuzy <bobuzytech@gmail.com>
2024-09-06 11:55:07 +0000

    Modify stylesheet and script links

--- web_dynamic/static/scripts/form.js
+++ web_dynamic/static/scripts/form.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', '../../components/form.html', true);
+    xhr.open('GET', '../../templates/form.html', true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             document.getElementById('form-placeholder').innerHTML = xhr.responseText;
