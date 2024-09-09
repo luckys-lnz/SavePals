644df0d7f650556c5c91f29951cfb71851779107
luckys-lnz <lalucky22@gmail.com>
2024-09-09 13:45:47 +0000

    savepals root

---
+++ web_dynamic/savepals.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/signup', methods=['GET'], strict_slashes=False )
+def get_index():
+    """Get the index page"""
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    # Replace 'dashboard.html' with the correct template for your dashboard
+    return render_template('dashboard.html')
+
+
+@app.route('/profile')
+def profile():
+    """Route to the user profile,and shows information of the user."""
+    return render_template('profile.html')      
+
+
+@app.route('/goals')
+def goals():
+    """Route to the user profile,and shows information of the user."""
+    return render_template('profile.html')      
+
+
+@app.route('/friends')
+def friends():
+    """Route to the user profile,and shows information of the user."""
+    return render_template('profile.html')      
+
+
+@app.route('/logout')
+def logout():
+    """Route to the user profile,and shows information of the user."""
+    return render_template('profile.html')      
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000, debug=True)
