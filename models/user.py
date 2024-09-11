e44defc85d773a3f389a52698e21fb0f5d577542
luckys-lnz <lalucky22@gmail.com>
2024-09-11 09:05:41 +0000

    updated usermodels

--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ class User(BaseModel, Base):
 
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    password = Column(String(1000), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
     phone = Column(String(128), nullable=False)
